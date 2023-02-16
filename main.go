9321c90d8882c7e45d6f198e5e87f16fb31b726a
Kevin Cantwell <kevin.cantwell@gmail.com>
2023-02-16 16:28:53 -0500

    update config structure

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -21,22 +22,37 @@ TODO:
 Usage: cliview [--config|-c CONFIG] [file|dir|url|-]
 */
 func main() {
-	maybeWriteDefaultConfig()
-	conf := loadConfig()
+	log.SetFlags(0)
+
+	conf, err := loadConfig()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 
 	arg := os.Args[1]
 
-	switch {
-	case isFile(arg):
-		handleFile(conf, arg)
+	configs, err := parseManyToOneConfigs(conf.FileTypes)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println("No view available for:\n", arg)
+
+	if isFile(arg) {
+		if n, err := handleFile(configs, arg); err != nil {
+			log.Println(err)
+			os.Exit(n)
+		} else {
+			os.Exit(0)
+		}
+	}
+
+	log.Println("No preview available for:\n", arg)
+	os.Exit(1)
 }
 
 type Config struct {
-	Mimes      yaml.MapSlice
-	Extensions yaml.MapSlice
-	Default    string
+	FileTypes yaml.MapSlice `yaml:"file_types"`
 }
 
 type MIME struct {
@@ -63,118 +79,156 @@ func parseMIME(s string) MIME {
 	}
 }
 
-func isFile(arg string) bool {
-	_, err := os.Stat(arg)
-	return !os.IsNotExist(err)
-}
-
-func handleFile(conf Config, arg string) {
-	abs := do(filepath.Abs(arg))
-	ext := filepath.Ext(arg)
-	if ext != "" {
-		handleFileByExtension(conf, abs, ext)
-		// if we get here, we didn't find a match, so fallthrough to mime handling
-	}
-	handleFileByMime(conf, abs)
-	// if we get here, we didn't find a match, so fallthrough to default handling
-	handleDefault(conf, abs)
-}
-
-func handleFileByExtension(conf Config, arg string, ext string) {
-	for _, item := range conf.Extensions {
-		exts, cmd := item.Key.(string), item.Value.(string)
-		ee := strings.Split(exts, ",")
-		for _, e := range ee {
-			e = strings.Trim(e, " ")
-			if e == ext {
-				exitCode := eval(cmd, arg)
-				os.Exit(exitCode)
-			}
+// parseManyToOneConfigs is a helper function to turn the configs into
+// an iterable list of command instructions.
+// Parses keys as a list of comma-separated strings and
+// maps each one to the command specified by the value
+func parseManyToOneConfigs(cmds yaml.MapSlice) ([][]string, error) {
+	var list [][]string
+	for _, item := range cmds {
+		types, ok := item.Key.(string)
+		if !ok {
+			return nil, fmt.Errorf("config key must be a string: %v", item.Key)
+		}
+		command, ok := item.Value.(string)
+		if !ok {
+			return nil, fmt.Errorf("config value must be a string: %v", item.Value)
+		}
+		typs := strings.Split(types, ",")
+		for _, t := range typs {
+			t = strings.Trim(t, " ")
+			list = append(list, []string{t, command})
 		}
 	}
+	return list, nil
 }
 
-func handleFileByMime(conf Config, arg string) {
-	// Use the `file` command to determine the mime type
-	output := do(exec.Command("file", "-b", "--mime-type", arg).Output())
-	mimeType := parseMIME(string(output))
+// Will return true if arg is a directory, file, or symlink
+func isFile(arg string) bool {
+	_, err := os.Lstat(arg)
+	return err == nil
+}
 
-	for _, item := range conf.Mimes {
-		mimes, cmd := item.Key.(string), item.Value.(string)
-		mm := strings.Split(mimes, ",")
-		for _, m := range mm {
-			// valid forms: "image/*", "image/jpeg", "video/mp4,video/quicktime"
-			m = strings.Trim(m, " ")
-			confType := parseMIME(m)
-			if confType.Type == "*" || confType.Type == mimeType.Type {
-				if confType.Subtype == "*" || confType.Subtype == mimeType.Subtype {
-					exitCode := eval(cmd, arg)
-					os.Exit(exitCode)
+func isMimeType(mime string) bool {
+	return strings.Count(mime, "/") == 1
+}
+
+func isExtension(ext string) bool {
+	return strings.HasPrefix(ext, ".")
+}
+
+func handleFile(configs [][]string, path string) (int, error) {
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		return 0, err
+	}
+	ext := filepath.Ext(path)
+
+	// Use the `file` command to determine the mime type of the
+	output, err := exec.Command("file", "-b", "--mime-type", abs).Output()
+	if err != nil {
+		return 0, err
+	}
+
+	log.Println(string(output))
+
+	fileMIME := parseMIME(string(output))
+
+	for _, item := range configs {
+		typ, cmd := item[0], item[1]
+
+		switch {
+		case isMimeType(typ):
+			confMIME := parseMIME(typ)
+			if confMIME.Type == "*" || confMIME.Type == fileMIME.Type {
+				if confMIME.Subtype == "*" || confMIME.Subtype == fileMIME.Subtype {
+					return eval(cmd, abs)
 				}
 			}
+		case isExtension(typ):
+			if typ == ext {
+				return eval(cmd, abs)
+			}
 		}
 	}
+
+	// if we get here, we didn't find a match, so fallthrough to default handling
+	return handleDefault(configs, abs)
 }
 
-func handleDefault(conf Config, arg string) {
-	exitCode := eval(conf.Default, arg)
-	os.Exit(exitCode)
+func handleDefault(configs [][]string, arg string) (int, error) {
+	for _, item := range configs {
+		typ, cmd := item[0], item[1]
+		if typ == "default" {
+			return eval(cmd, arg)
+		}
+	}
+	return 0, nil
 }
 
-func eval(cmd string, arg string) int {
+func eval(cmd string, arg string) (int, error) {
 	cmd = strings.ReplaceAll(cmd, "{}", arg)
 	shell := os.Getenv("SHELL")
 	if shell == "" {
 		shell = "sh"
 	}
+
 	c := exec.Command(shell, "-c", cmd)
 	c.Stderr = os.Stderr
 	c.Stdout = os.Stdout
 	if err := c.Run(); err != nil {
+		log.Println("error executing command:", cmd)
 		if exitErr, ok := err.(*exec.ExitError); ok {
-			return exitErr.ExitCode()
+			return exitErr.ExitCode(), err
 		}
-		panic(err)
+		return 1, err
 	}
-	return 0
+	return 0, nil
 }
 
-func maybeWriteDefaultConfig() {
-	home := do(os.UserHomeDir())
-	_, err := os.Stat(home + "/.cliview/config.yml")
-	if !os.IsNotExist(err) {
-		return
+func maybeWriteDefaultConfig() error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(home + "/.cliview/config.yml"); !os.IsNotExist(err) {
+		// if the file already exists, don't overwrite it
+		return nil
 	}
 	if err := os.MkdirAll(home+"/.cliview", 0755); err != nil {
-		panic(err)
+		return err
 	}
-	f := do(os.Create(home + "/.cliview/config.yml"))
-	n := do(f.Write(config))
-	if n != len(config) {
-		panic("failed to write config")
-	}
-}
-
-func loadConfig() Config {
-	home := do(os.UserHomeDir())
-	configYML := do(os.Open(home + "/.cliview/config.yml"))
-	var conf Config
-	if err := yaml.Unmarshal(do(io.ReadAll(configYML)), &conf); err != nil {
-		fmt.Printf("%+v\n", conf)
-		panic(err)
-	}
-	return conf
-}
-
-func replaceArg(arg string, cmd string) string {
-	return strings.ReplaceAll(cmd, "{}", arg)
-}
-
-// do is a generic function that will accept any type T and an error,
-// handle the error, then return T alone.
-func do[T any](t T, err error) T {
+	f, err := os.Create(home + "/.cliview/config.yml")
 	if err != nil {
-		panic(err)
+		return err
 	}
-	return t
+	if _, err := f.Write(config); err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadConfig() (*Config, error) {
+	if err := maybeWriteDefaultConfig(); err != nil {
+		return nil, err
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	configYML, err := os.Open(homeDir + "/.cliview/config.yml")
+	if err != nil {
+		return nil, err
+	}
+	bb, err := io.ReadAll(configYML)
+	if err != nil {
+		return nil, err
+	}
+	var conf Config
+	if err := yaml.Unmarshal(bb, &conf); err != nil {
+		return nil, err
+	}
+	return &conf, nil
 }
